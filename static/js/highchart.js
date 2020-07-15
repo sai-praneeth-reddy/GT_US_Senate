1ac8cd098cebaf73c7de982fb7f966d2880cca73
sai-praneeth-reddy <praneeth09m248@gmail.com>
2020-07-14 20:06:12 -0400

    Updated Highchart

--- static/js/highchart.js
+++ static/js/highchart.js
@@ -14,8 +14,8 @@
 // var categories = "Politician";
 
 
-d3.csv("./data/top10_approve_rate.csv").then(function(approve) {
-    console.log(approve);
+d3.csv("/data/top10_approve_rate.csv").then(function(approve) {
+  console.log(approve);
 
 // d3.csv("/data/employees.csv", function(data) {
 //     for (var i = 0; i < data.length; i++) {
@@ -24,13 +24,13 @@ d3.csv("./data/top10_approve_rate.csv").then(function(approve) {
 //     }
 // });
 
-    var Politician = [],
-        Approve = [],
-        Disapprove = [];
+  var Politician = [],
+      Approve = [],
+      Disapprove = [];
 approve.map(function(d) {
-    Politician.push(d.Politician);
-    Approve.push(d.Approve);
-    Disapprove.push(d.Disapprove);
+  Politician.push(d.Politician);
+  Approve.push(d.Approve);
+  Disapprove.push(d.Disapprove);
 })
 
 
@@ -38,87 +38,87 @@ console.log(Politician);
 
 
 Highcharts.chart('container', {
-    chart: {
-      type: 'bar'
-    },
-    title: {
-      text: "Q4 2018 Rankings"
-    },
-    subtitle: {
-      text: 'Source: <a href="https://morningconsult.com/2019/01/10/americas-most-and-least-popular-senators-q4-2018-2/">MorningConsult</a>'
+  chart: {
+    type: 'bar'
+  },
+  title: {
+    text: "Q4 2018 Rankings"
+  },
+  subtitle: {
+    text: 'Source: <a href="https://morningconsult.com/2019/01/10/americas-most-and-least-popular-senators-q4-2018-2/">MorningConsult</a>'
+  },
+  accessibility: {
+    point: {
+      valueDescriptionFormat: '{index}. Approve {xDescription}, {value}%.'
+    }
+  },
+  xAxis: [{
+    categories: Politician,
+    // reversed: false,
+    labels: {
+      step: 1
     },
     accessibility: {
-      point: {
-        valueDescriptionFormat: '{index}. Approve {xDescription}, {value}%.'
-      }
+      description: 'Approve'
+    }
+  }, { // mirror axis on right side
+    opposite: true,
+    // reversed: false,
+    categories: Politician,
+    linkedTo: 0,
+    labels: {
+      step: 1
     },
-    xAxis: [{
-      categories: Politician,
-      reversed: false,
-      labels: {
-        step: 1
-      },
-      accessibility: {
-        description: 'Approve'
-      }
-    }, { // mirror axis on right side
-      opposite: true,
-      reversed: false,
-      categories: Politician,
-      linkedTo: 0,
-      labels: {
-        step: 1
-      },
-      accessibility: {
-        description: 'Disapprove'
-      }
-    }],
-    yAxis: {
-      title: {
-        text: null
-      },
-      labels: {
-        formatter: function () {
-          return Math.abs(this.value) + '%';
-        }
-      },
-      accessibility: {
-        description: 'Percentage population',
-        rangeDescription: 'Range: 0 to 100%'
-      }
+    accessibility: {
+      description: 'Disapprove'
+    }
+  }],
+  yAxis: {
+    title: {
+      text: null
     },
-  
-    plotOptions: {
-      series: {
-        stacking: 'normal'
-      }
-    },
-  
-    tooltip: {
+    labels: {
       formatter: function () {
-        return '<b>' + this.series.name + ', senator ' + this.point.category + '</b><br/>' +
-          'Percentage: ' + Highcharts.numberFormat(Math.abs(this.point.y), 1) + '%';
+        return Math.abs(this.value) + '%';
       }
     },
-  
-    series: [{
-      name: 'Approve',
-      data: [
-        -64, -62, -62, -59, -58,
-        -58, -57, -57, -56, -56
-      ]
-    }, {
-      name: 'Disapprove',
-      data: [
-        28, 26, 23, 27, 23,
-        27, 31, 29, 29, 27
-      ]
-    }]
-  });
+    accessibility: {
+      description: 'Percentage population',
+      rangeDescription: 'Range: 0 to 100%'
+    }
+  },
 
-  console.log(Approve)
+  plotOptions: {
+    series: {
+      stacking: 'normal'
+    }
+  },
 
-  });
+  tooltip: {
+    formatter: function () {
+      return '<b>' + this.series.name + ', senator ' + this.point.category + '</b><br/>' +
+        'Percentage: ' + Highcharts.numberFormat(Math.abs(this.point.y), 1) + '%';
+    }
+  },
+
+  series: [{
+    name: 'Approve',
+    data: [
+      -64, -62, -62, -59, -58,
+      -58, -57, -57, -56, -56
+    ]
+  }, {
+    name: 'Disapprove',
+    data: [
+      28, 26, 23, 27, 23,
+      27, 31, 29, 29, 27
+    ]
+  }]
+});
+
+console.log(Approve)
+
+});
 
 
   
